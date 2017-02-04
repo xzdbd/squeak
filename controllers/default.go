6442e68a723d5ab0b1cfadc73797ce3c9bf54276
xzdbd <xzdbd@sina.com>
2017-02-04 18:02:29 +0800

    add pollution web page.

--- controllers/default.go
+++ controllers/default.go
@@ -8,6 +8,14 @@ type MainController struct {
 	beego.Controller
 }
 
+type PollutionController struct {
+	beego.Controller
+}
+
 func (c *MainController) Get() {
 	c.TplName = "index.html"
+}
+
+func (c *PollutionController) Get() {
+	c.TplName = "pollution/index.html"
 }
